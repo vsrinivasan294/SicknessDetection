4942a5c9951b46dd7b1bec503091930dbb879b98
Vishnu Srinivasan <vishnunaga_srinivasan@gmail.com>
2018-10-23 21:35:04 -0500

    Added the basics of the website

---
+++ alpha/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import FPM from './FPM.png';
+import Malaria from './Malaria.png';
+import SickleCell from './SickleCell.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Redefining Blood Tests.
+          </p>
+          <p>
+            Quanto brings reliable, cheap and fast blood testing to consumers. 
+          </p>
+          <p>
+            Our Technology
+          </p>
+          
+          {/* Adding Images here and describing them */}
+
+          <p>
+            Blood Cell Count. 
+          </p>
+          <img src={FPM} className="Our-Technology" alt="FPM" />
+          
+          <p>
+            Malaria Detection. 
+          </p>
+          <img src={Malaria} className="Our-Technology" alt="Malaria" />
+          
+          <p>
+            SickleCell Detection. 
+          </p>
+          <img src={SickleCell} className="Our-Technology" alt="SickleCell" />
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
