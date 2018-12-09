8e3d79375daa64a9444b1443e81080d2dbe8134d
Jason <jasonyaoyifei@gmail.com>
2018-12-09 16:04:05 -0600

    fixed picture issue

--- flaskProject/InfoBlog/routes.py
+++ flaskProject/InfoBlog/routes.py
@@ -78,7 +78,7 @@ def logout():
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = current_user.username + f_ext
+    picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/images/blood_samples', picture_fn)
     form_picture.save(picture_path)
 
